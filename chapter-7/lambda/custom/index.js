f3cf5a487a2189e26dbea547f80cf701699bfdde
Dustin Coates <dustin@dcoates.com>
2018-02-26 00:23:52 +0100

    Add code to render cards

--- chapter-7/lambda/custom/index.js
+++ chapter-7/lambda/custom/index.js
@@ -5,13 +5,19 @@ const languageStrings = {
     translation: {
       SpaceFact: "The first American in space was Alan Shepard.",
       PlanetDistance: "The planet {{name}} is {{km}} kilometers," +
-                      " or {{mi}} miles, from the Sun."
+                      " or {{mi}} miles, from the Sun.",
+      PlanetDistanceCardTitle: "Distance Between {{name}} and the Sun",
+      PlanetDistanceCardContent: "At its closest, {{name}} is {{km}}" +
+                                 " kilometers, or {{mi}} miles, from the Sun."
     }
   },
   "en-GB": {
     translation: {
       SpaceFact: "The first Briton in space was Helen Sharman.",
-      PlanetDistance: "The planet {{name}} is {{km}} kilometers from the Sun."
+      PlanetDistance: "The planet {{name}} is {{km}} kilometres from the Sun.",
+      PlanetDistanceCardTitle: "Distance Between {{name}} and the Sun",
+      PlanetDistanceCardContent: "At its closest, {{name}} is {{km}}" +
+                                 " kilometres from the Sun."
     }
   },
   fallback: {
@@ -25,41 +31,73 @@ const planets = {
     name: "Mercury",
     km: 69816900,
     mi: 43400000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/mercury_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/mercury_large_card.jpg"
+    }
   },
   venus: {
     name: "Venus",
     km: 108939000,
-    mi: 67700000
+    mi: 67700000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/venus_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/venus_large_card.jpg"
+    }
   },
   earth: {
     name: "Earth",
     km: 152100000,
-    mi: 94500000
+    mi: 94500000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/earth_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/earth_large_card.jpg"
+    }
   },
   mars: {
     name: "Mars",
     km: 249200000,
-    mi: 155000000
+    mi: 155000000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/mars_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/mars_large_card.jpg"
+    }
   },
   jupiter: {
     name: "Jupiter",
     km: 816620000,
-    mi: 507000000
+    mi: 507000000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/jupiter_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/jupiter_large_card.jpg"
+    }
   },
   saturn: {
     name: "Saturn",
     km: 1514500000,
-    mi: 941000000
+    mi: 941000000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/saturn_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/saturn_large_card.jpg"
+    }
   },
   uranus: {
     name: "Uranus",
     km: 3000000000,
-    mi: 1869000000
+    mi: 1869000000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/uranus_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/uranus_large_card.jpg"
+    }
   },
   neptune: {
     name: "Neptune",
     km: 4537000000,
-    mi: 2819000000
+    mi: 2819000000,
+    img: {
+      smallImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/neptune_small_card.jpg",
+      largeImageUrl: "https://s3.amazonaws.com/skills-resources/hello-science/neptune_large_card.jpg"
+    }
   }
 };
 
@@ -96,6 +134,12 @@ const handlers = {
       const planet = planets[values[0].value.id.toLowerCase()];
 
       speech = this.t("PlanetDistance", planet);
+
+      const cardTitle = this.t("PlanetDistanceCardTitle", planet);
+      const cardContent = this.t("PlanetDistanceCardContent", planet);
+      const cardImage = planet.img;
+
+      this.response.cardRenderer(cardTitle, cardContent, cardImage);
     } else {
       speech = this.t(["UnknownPlanet", languageStrings.fallback.UnknownPlanet]);
     }
