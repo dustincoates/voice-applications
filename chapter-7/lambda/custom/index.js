98b6c5eb63ad3908d1e6e0d802984bf068fd94f3
Dustin Coates <dustin@dcoates.com>
2018-02-25 22:43:36 +0100

    Add localized fact intent and LaunchRequest

--- chapter-7/lambda/custom/index.js
+++ chapter-7/lambda/custom/index.js
@@ -1,55 +1,35 @@
-'use strict';
-var Alexa = require("alexa-sdk");
+const Alexa = require("alexa-sdk");
 
-// For detailed tutorial on how to making a Alexa skill,
-// please visit us at http://alexa.design/build
-
-
-exports.handler = function(event, context) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+const languageStrings = {
+  "en-US": {
+    translation: {
+      SpaceFact: "The first American in space was Alan Shepard."
+    }
+  },
+  "en-GB": {
+    translation: {
+      SpaceFact: "The first Briton in space was Helen Sharman."
+    }
+  }
 };
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello');
-    },
-    'MyNameIsIntent': function () {
-        this.emit('SayHelloName');
-    },
-    'SayHello': function () {
-        this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
-        this.emit(':responseReady');
-    },
-    'SayHelloName': function () {
-        var name = this.event.request.intent.slots.name.value;
-        this.response.speak('Hello ' + name)
-            .cardRenderer('hello world', 'hello ' + name);
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest' : function() {
-        console.log('Session ended with reason: ' + this.event.request.reason);
-    },
-    'AMAZON.StopIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-            " name is awesome Aaron'");
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't get that. You can try: 'alexa, hello world'" +
-            " or 'alexa, ask hello world my name is awesome Aaron'");
-    }
+const handlers = {
+  LaunchRequest () {
+    this.emit("GetFactIntent");
+  },
+  GetFactIntent () {
+    this.response.speak(this.t("SpaceFact"));
+    this.emit(":responseReady");
+  },
+  Unhandled () {
+    this.response.speak(this.t("Unhandled"));
+    this.emit(":responseReady");
+  }
+};
+
+exports.handler = function(event, context) {
+  const alexa = Alexa.handler(event, context);
+  alexa.resources = languageStrings;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
