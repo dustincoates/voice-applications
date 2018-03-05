1c8b7c974b1e8b697eb27a7e86615a0aec1bb3ec
Dustin Coates <dustin@dcoates.com>
2018-03-05 22:07:31 +0100

    Add fulfillment for BuyTicketsIntent

--- chapter-8/lambda/custom/index.js
+++ chapter-8/lambda/custom/index.js
@@ -1,55 +1,58 @@
-'use strict';
-var Alexa = require("alexa-sdk");
+"use strict";
+const Alexa = require("alexa-sdk");
 
-// For detailed tutorial on how to making a Alexa skill,
-// please visit us at http://alexa.design/build
-
-
-exports.handler = function(event, context) {
-    var alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+const languageStrings = {
+  "en-US": {
+    translation: {
+      BuyTickets: "Alright, {{NumberTickets}} tickets for {{MovieName}}" +
+                  " coming up! That'll be ${{Price}} in all."
+    }
+  },
+  fallback: {
+    Unhandled: "I can't do that at the moment! How about buying tickets?",
+    UnhandledPrompt: "Tell me the movie you want to see."
+  }
 };
 
-var handlers = {
-    'LaunchRequest': function () {
-        this.emit('SayHello');
-    },
-    'HelloWorldIntent': function () {
-        this.emit('SayHello');
-    },
-    'MyNameIsIntent': function () {
-        this.emit('SayHelloName');
-    },
-    'SayHello': function () {
-        this.response.speak('Hello World!')
-                     .cardRenderer('hello world', 'hello world');
-        this.emit(':responseReady');
-    },
-    'SayHelloName': function () {
-        var name = this.event.request.intent.slots.name.value;
-        this.response.speak('Hello ' + name)
-            .cardRenderer('hello world', 'hello ' + name);
-        this.emit(':responseReady');
-    },
-    'SessionEndedRequest' : function() {
-        console.log('Session ended with reason: ' + this.event.request.reason);
-    },
-    'AMAZON.StopIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'AMAZON.HelpIntent' : function() {
-        this.response.speak("You can try: 'alexa, hello world' or 'alexa, ask hello world my" +
-            " name is awesome Aaron'");
-        this.emit(':responseReady');
-    },
-    'AMAZON.CancelIntent' : function() {
-        this.response.speak('Bye');
-        this.emit(':responseReady');
-    },
-    'Unhandled' : function() {
-        this.response.speak("Sorry, I didn't get that. You can try: 'alexa, hello world'" +
-            " or 'alexa, ask hello world my name is awesome Aaron'");
+const handlers = {
+  LaunchRequest () {
+    this.emit("BuyTicketsIntent");
+  },
+  BuyTicketsIntent () {
+    if(this.event.request.dialogState !== "COMPLETED"){
+      this.emit(":delegate");
+    } else {
+      const movie = {
+        NumberTickets: parseFloat(this.event.request.intent.slots.NumberTickets.value),
+        MovieName: this.event.request.intent.slots.MovieName.value
+      };
+
+      movie.Price = movie.NumberTickets * 10;
+
+      if(this.event.request.intent.slots.Discounts.value) {
+        movie.Price = movie.Price / 2;
+      }
+
+      const speak = this.t("BuyTickets", movie);
+      this.response.speak(speak);
+      this.emit(":responseReady");
     }
+  },
+  Unhandled () {
+    const speak = this.t(["Unhandled", languageStrings.fallback.Unhandled]);
+    const listen = this.t([
+      "UnhandledPrompt", languageStrings.fallback.UnhandledPrompt
+    ]);
+    this.response
+      .speak(speak)
+      .listen(listen);
+    this.emit(":responseReady");
+  }
+};
+
+exports.handler = function(event, context) {
+  const alexa = Alexa.handler(event, context);
+  alexa.resources = languageStrings;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
