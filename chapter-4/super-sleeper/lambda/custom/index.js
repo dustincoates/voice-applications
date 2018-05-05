325657e0c66b5d83b3fcaf98ff5e5c97dbf9e6f2
Dustin Coates <dustin@dcoates.com>
2018-05-05 14:44:10 +0200

    Update fulfillment

--- chapter-4/super-sleeper/lambda/custom/index.js
+++ chapter-4/super-sleeper/lambda/custom/index.js
@@ -1,104 +1,140 @@
-/* eslint-disable  func-names */
-/* eslint-disable  no-console */
+const Alexa = require("ask-sdk");
 
-const Alexa = require('ask-sdk-core');
+function pluck(arr) {
+  const randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+}
 
-const LaunchRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
-
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
+const WellRestedPhrases = {
+  tooMuch: [
+    "I think you may sleep too much and swing back to tired.",
+    "whoa, that's a lot of sleep. You'll wake up rested for sure."
+  ],
+  justRight: [
+    "you should wake up refreshed.",
+    "it's clear you know rest is important. Good job, you.",
+    "with that much sleep, you're ready to face the world.",
+    "you'll wake up invigorated."
+  ],
+  justUnder: [
+    "you may get by, but watch out for a mid-day crash.",
+    "you'll be alright, but would be better off with a bit more time.",
+    "you might be a little tired tomorrow."
+  ],
+  tooLittle: [
+    "you'll be dragging tomorrow. Get the coffee ready!",
+    "that's either a long night or early morning? Either" +
+      "way, tomorrow's going to be rough."
+  ]
 };
 
-const HelloWorldIntentHandler = {
+const WellRestedIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+    const intentName = "WellRestedIntent";
+
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === intentName;
   },
   handle(handlerInput) {
-    const speechText = 'Hello World!';
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
-  },
+    const numOfHours = slots.NumberOfHours.value;
+    let adjustedHours = parseInt(numOfHours);
+
+    const resolutionValues = slots.SleepQuality &&
+      slots.SleepQuality.resolutions &&
+      slots.SleepQuality.resolutions.resolutionsPerAuthority[0] &&
+      slots.SleepQuality.resolutions.resolutionsPerAuthority[0].values;
+
+    if (Number.isInteger(adjustedHours)) {
+      let speech = "";
+
+      if (resolutionValues) {
+        const quality = resolutionValues[0].value.id;
+        
+        if (quality === "good") {
+          adjustedHours += 1;
+          speech = "You slept well last night, and ";
+        }
+
+        if (quality === "bad") {
+          adjustedHours -= 1;
+          speech = "You slept poorly last night, and ";
+        }
+      }
+
+      if (adjustedHours > 12) {
+        speech += pluck(WellRestedPhrases.tooMuch);
+      } else if (adjustedHours > 8) {
+        speech += pluck(WellRestedPhrases.justRight);
+      } else if (adjustedHours > 6) {
+        speech += pluck(WellRestedPhrases.justUnder);
+      } else {
+        speech += pluck(WellRestedPhrases.tooLittle);
+      }
+
+      return handlerInput.responseBuilder.speak(speech).getResponse();
+    } else {
+      console.log("Slot values ", slots);
+
+      const speech =
+        "Oh, I don't know what happened. Tell me again. " +
+        "How many hours will you sleep tonight?";
+      const reprompt = "How many hours are you going to sleep tonight?";
+
+      return handlerInput.responseBuilder
+        .speak(speech)
+        .reprompt(reprompt)
+        .getResponse();
+    }
+  }
 };
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    const intentName = "AMAZON.HelpIntent";
+
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === intentName;
   },
   handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    const speech = "You can ask Super Sleeper how well rested you'll be or " +
+                    "share how well you slept the night before. Try saying " +
+                    "'How tired will I be if sleeping 8 hours.'";
+    const reprompt = "You can also say something like, '5 hours of sleep " +
+                     "and slept well last night'";
 
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .speak(speech)
+      .reprompt(reprompt)
       .getResponse();
-  },
+  }
 };
 
-const CancelAndStopIntentHandler = {
+const StopOrCancelIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    const stopIntentName = "AMAZON.StopIntent";
+    const cancelIntentName = "AMAZON.CancelIntent";
+
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      (handlerInput.requestEnvelope.request.intent.name === stopIntentName ||
+      handlerInput.requestEnvelope.request.intent.name === cancelIntentName);
   },
   handle(handlerInput) {
-    const speechText = 'Goodbye!';
+    const speech = "Alright, see you around and sleep well.";
 
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .speak(speech)
       .getResponse();
-  },
+  }
 };
 
-const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
-
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
-
-const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
-
-    return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
-      .getResponse();
-  },
-};
-
-const skillBuilder = Alexa.SkillBuilders.custom();
-
-exports.handler = skillBuilder
+const skillId = "<YOUR SKILL ID>";
+exports.handler = Alexa.SkillBuilders.standard()
   .addRequestHandlers(
-    LaunchRequestHandler,
-    HelloWorldIntentHandler,
+    WellRestedIntentHandler,
     HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
+    StopOrCancelIntentHandler
   )
-  .addErrorHandlers(ErrorHandler)
+  .withSkillId(skillId)
   .lambda();
