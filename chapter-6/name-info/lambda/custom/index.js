f934a0416e886fc43786fea33b8c3a1d081d5390
Dustin Coates <dustin@dcoates.com>
2018-05-12 21:46:18 +0200

    Add handlers

--- chapter-6/name-info/lambda/custom/index.js
+++ chapter-6/name-info/lambda/custom/index.js
@@ -1,104 +1,158 @@
-/* eslint-disable  func-names */
-/* eslint-disable  no-console */
+const Alexa = require("ask-sdk");
+const AWS = require("aws-sdk");
 
-const Alexa = require('ask-sdk-core');
-
-const LaunchRequestHandler = {
+const GetNameIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
-  },
-  handle(handlerInput) {
-    const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+    const intentName = "GetNameIntent";
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === intentName;
+  },
+  async handle(handlerInput) {
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const name = slots.name.value;
+
+    const data = await handlerInput
+                        .attributesManager
+                        .getPersistentAttributes();
+    
+    data.name = name;
+
+    if(name) {
+      handlerInput.attributesManager.setPersistentAttributes(data);
+      await handlerInput.attributesManager.savePersistentAttributes(data);
+
+      const speech = name + " sure is a nice name. " +
+                     "What do you want to know about it?";
+      const reprompt = "I could spell it for you.";
+
+      return handlerInput.responseBuilder
+        .speak(speech)
+        .reprompt(reprompt)
+        .getResponse();
+    } else {
+      return LaunchRequestHandler.handle(handlerInput);
+    }
   },
 };
 
-const HelloWorldIntentHandler = {
+const SpellingIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
-  },
-  handle(handlerInput) {
-    const speechText = 'Hello World!';
+    const intentName = "SpellingIntent";
 
-    return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
-      .getResponse();
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === intentName;
+  },
+  async handle(handlerInput) {
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const name = slots.name.value;
+
+    const data = await handlerInput
+                        .attributesManager
+                        .getPersistentAttributes();
+
+    if(name) {
+      data.name = name;
+    }
+
+    if(data.name) {
+      handlerInput.attributesManager.setPersistentAttributes(data);
+      await handlerInput.attributesManager.savePersistentAttributes(data);
+      
+      const speech = `You spell ${data.name}, ` +
+                      data.name.split("").join(" ");
+                      
+      return handlerInput.responseBuilder
+        .speak(speech)
+        .getResponse();
+    } else {
+      return LaunchRequestHandler.handle(handlerInput);
+    }
   },
 };
 
 const HelpIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    const intentName = "AMAZON.HelpIntent";
+
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      handlerInput.requestEnvelope.request.intent.name === intentName;
   },
   handle(handlerInput) {
-    const speechText = 'You can say hello to me!';
+    const speech =
+      "Ask me for a first name to get some facts about it.";
+    const reprompt =
+      "Also say something like, 'What is the first letter of Joanna?'";
 
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .reprompt(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .speak(speech)
+      .reprompt(reprompt)
       .getResponse();
-  },
+  }
 };
 
-const CancelAndStopIntentHandler = {
+const StopOrCancelIntentHandler = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
-      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
-        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    const stopIntentName = "AMAZON.StopIntent";
+    const cancelIntentName = "AMAZON.CancelIntent";
+
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+      (handlerInput.requestEnvelope.request.intent.name === stopIntentName ||
+        handlerInput.requestEnvelope.request.intent.name === cancelIntentName);
   },
   handle(handlerInput) {
-    const speechText = 'Goodbye!';
+    const speech = "Catch you on the flip side.";
+
+    return handlerInput.responseBuilder.speak(speech).getResponse();
+  }
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === "LaunchRequest";
+  },
+  handle(handlerInput) {
+    const speech =
+      "The name info skill needs you to just ask for a first name.";
+    const reprompt = "Try saying, 'give me facts about the name Dustin.'";
 
     return handlerInput.responseBuilder
-      .speak(speechText)
-      .withSimpleCard('Hello World', speechText)
+      .speak(speech)
+      .reprompt(reprompt)
       .getResponse();
-  },
+  }
 };
 
-const SessionEndedRequestHandler = {
+const Unhandled = {
   canHandle(handlerInput) {
-    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
-
-    return handlerInput.responseBuilder.getResponse();
-  },
-};
-
-const ErrorHandler = {
-  canHandle() {
     return true;
   },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+  handle(handlerInput) {
+    const speech = "Oh, I think I misunderstand. " +
+                   "Can you try once more?";
+    const reprompt = "How about this. Tell me a first name.";
 
     return handlerInput.responseBuilder
-      .speak('Sorry, I can\'t understand the command. Please say again.')
-      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .speak(speech)
+      .reprompt(reprompt)
       .getResponse();
   },
 };
 
-const skillBuilder = Alexa.SkillBuilders.custom();
-
-exports.handler = skillBuilder
+const skillId = "<YOUR SKILL ID>";
+exports.handler = Alexa.SkillBuilders.standard()
   .addRequestHandlers(
-    LaunchRequestHandler,
-    HelloWorldIntentHandler,
+    GetNameIntentHandler,
+    SpellingIntentHandler,
     HelpIntentHandler,
-    CancelAndStopIntentHandler,
-    SessionEndedRequestHandler
+    StopOrCancelIntentHandler,
+    LaunchRequestHandler,
+    Unhandled
   )
-  .addErrorHandlers(ErrorHandler)
+  .withSkillId(skillId)
+  .withTableName("name_info")
+  .withAutoCreateTable(true)
+  .withDynamoDbClient(
+    new AWS.DynamoDB({ apiVersion: "latest", region: "us-east-1" })
+  )
   .lambda();
