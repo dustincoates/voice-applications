2c30ef9a9ee8ca030ef8b56e7e1a8c0b912aa38b
Dustin Coates <dustin@dcoates.com>
2018-06-10 20:56:51 +0200

    Add initial fulfillment for Actions SDK app

--- chapter-10/exhibit-engine/functions/index.js
+++ chapter-10/exhibit-engine/functions/index.js
@@ -1,8 +1,25 @@
-const functions = require('firebase-functions');
+const {actionssdk} = require("actions-on-google");
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = actionssdk({debug: true});
+
+app.intent("exhibitEngine.HELP", (conv) => {
+  const response = "Help is what I'm here for. " + 
+                   "Exhibit Engine has a couple apps. Calculator will " +
+                   "calculate two numbers. Echo will repeat what you say. " +
+                   "You can try it by saying something like, 'engage echo " +
+                   "to repeat well, that just beats all.'";
+  
+  conv.close(response);
+});
+
+app.intent("actions.intent.MAIN", (conv) => {
+  const response = "Hey there! With Exhibit Engine, you can talk to other " +
+                   "apps. Just say 'engage' and then what you wanna do. " +
+                   "Like... engage calculator to add one plus one. " +
+                   "Go ahead. I'm listening.";
+  
+  conv.ask(response);
+});
+
+exports.exhibitEngineApp = functions.https.onRequest(app);
