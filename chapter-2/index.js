b2ead9fbc0718254fd0c5e2329df933582d5eea2
Dustin Coates <dustin@dcoates.com>
2018-10-14 02:32:36 +0200

    Add WellRestedIntentHandler

---
+++ chapter-2/index.js
@@ -0,0 +1,22 @@
+const Alexa = require("ask-sdk");
+ 
+const WellRestedIntentHandler = {
+  canHandle(handlerInput) {
+    const intentName = "WellRestedIntent";
+ 
+    return handlerInput.requestEnvelope.request.type === "IntentRequest" &&
+           handlerInput.requestEnvelope.request.intent.name === intentName;
+  },
+  handle(handlerInput) {
+    const speech = "Well rested intent";
+ 
+    return handlerInput
+            .responseBuilder
+            .speak(speech)
+            .getResponse();
+  }
+};
+
+exports.handler = Alexa.SkillBuilders.standard()
+                    .addRequestHandlers(WellRestedIntentHandler)
+                    .lambda();
